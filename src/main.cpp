30134e5f1bbf17069274b1c2a1daa8cd3ca8c263
TomasSG <tomassanchezgrigioni98@gmail.com>
2021-06-13 13:09:04 -0300

    Feature prender led finalizada

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
 
+#define PIN_LED 4
+#define TIEMPO_MILIS 1000
+
+int led_estado = LOW;
+
 void setup() {
-  // put your setup code here, to run once:
+  // Poner el pin en modo salida
+  pinMode(PIN_LED, OUTPUT);
+
+  // Establecer el pin en low
+  led_estado = LOW;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Cambiar el estado del led
+  led_estado = !led_estado;
+
+  // Actualizar el pin
+  digitalWrite(PIN_LED, led_estado);
+
+  // Esperamos un tiempo
+  delay(TIEMPO_MILIS);
 }
