80773cc308260d48315c9890e1fff07420853cda
TomasSG <tomassanchezgrigioni98@gmail.com>
2021-06-23 12:28:14 -0300

    Agregamos estados y eventos nuevos

--- include/main.h
+++ include/main.h
@@ -10,24 +10,29 @@
 #define ST_INACTIVO                     0
 #define ST_REALIZANDO_CONEXION_WIFI     1
 #define ST_CONECTADO_WIFI               2
-#define ST_DETECTANDO_OBJETO            3
+#define ST_REALIZANDO_CONEXION_FB       3
+#define ST_CONECTADO_FB                 4
+#define ST_DETECTANDO_OBJETO            5
 
 // Estados MdE sensores distancia
-#define ST_OBJETO_NO_DETECTADO          4
-#define ST_OBJETO_DETECTADO             5
+#define ST_OBJETO_NO_DETECTADO          6
+#define ST_OBJETO_DETECTADO             7
 
 /* ------------------ EVENTOS ------------------ */
 
 // Eventos MdE general
 #define EVT_CONTINUAR                   0
-#define EVT_ACABA_TIEMPO                1
-#define EVT_CONEXION_EXITOSA            2
-#define EVT_DESCONEXION                 3
-#define EVT_COMENZAR_DETECCION          4
+#define EVT_ACABA_TIEMPO_WIFI           1
+#define EVT_CONEXION_EXITOSA_WIFI       2
+#define EVT_DESCONEXION_WIFI            3
+#define EVT_ACABA_TIEMPO_FB             4
+#define EVT_CONEXION_EXITOSA_FB         5
+#define EVT_DESCONEXION_FB              6
+#define EVT_COMENZAR_DETECCION          7
 
 // Eventos MdE sensores distancia
-#define EVT_OBJETO_FUERA_RANGO          5
-#define EVT_OBJETO_DENTRO_RANGO         6
+#define EVT_OBJETO_FUERA_RANGO          8
+#define EVT_OBJETO_DENTRO_RANGO         9
 
 /* ------------------ DECLARACIÓN FUNCIONES ------------------ */
 
