ff8c9a99bb08188dadedcd0b66d28f1fdf315816
TomasSG <tomassanchezgrigioni98@gmail.com>
2021-07-21 17:50:14 -0300

    Agregue funcion calcular promedio

--- lib/sensores/sensores.cpp
+++ lib/sensores/sensores.cpp
@@ -66,3 +66,41 @@ float obtenerNivelBateria(int pin, float vMin, float vMax, float constCorr) {
     return porcentaje;
   }
 }
+
+/*
+* Dado un array con los valores leidos, lo ordena, filtra las primeras y últimas
+* n observaciones porque se las trata como ruido y calcula el promedio con
+* los restantes.
+*/
+
+float calcularPromedio(float *valores, int len) {
+
+  // Verificar tener por lo menos 11 observaciones
+  if(len <= 10) {
+    return ERROR_PROMEDIO;
+  }
+  
+  // Inicializar variables
+  float acum = 0;
+  
+  // Ordenar el vector de forma ascendente
+  for(int i = 0; i < len; i++) {
+    for(int j = i + 1; j < len - 1; j++) {
+      if(valores[i] > valores[j]) {
+        float swap = valores[i];
+        valores[i] = valores[j];
+        valores[j] = swap;
+      }
+    }
+
+    // Calcular el promedio excluyendo las primeras y últimas 5 observaciones,
+    // se consideran que son ruido.
+    for(int i = 5; i < len - 5; i++) {
+      acum += valores[i];
+    }
+
+    return acum / len - 10;
+
+  }
+
+}
